3dbc2d3fe977dc82d350b7ca6d8039bc7060fcf0
chicn <chihiros92@suou.waseda.jp>
2017-04-28 20:01:02 +0900

    Run first time

--- main.py
+++ main.py
@@ -5,44 +5,46 @@ import cv2
 import os
 import time
 
-detector = MtcnnDetector(model_folder='model', ctx=mx.cpu(0), num_worker = 4 , accurate_landmark = False)
+detector = MtcnnDetector(model_folder='model', ctx=mx.cpu(4), num_worker = 4 , accurate_landmark = False)
 
 
-img = cv2.imread('test2.jpg')
+# img = cv2.imread('test2.jpg')
 
-# run detector
-results = detector.detect_face(img)
+# # run detector
+# results = detector.detect_face(img)
 
-if results is not None:
+# if results is not None:
 
-    total_boxes = results[0]
-    points = results[1]
-    
-    # extract aligned face chips
-    chips = detector.extract_image_chips(img, points, 144, 0.37)
-    for i, chip in enumerate(chips):
-        cv2.imshow('chip_'+str(i), chip)
-        cv2.imwrite('chip_'+str(i)+'.png', chip)
+#     total_boxes = results[0]
+#     points = results[1]
 
-    draw = img.copy()
-    for b in total_boxes:
-        cv2.rectangle(draw, (int(b[0]), int(b[1])), (int(b[2]), int(b[3])), (255, 255, 255))
+#     # extract aligned face chips
+#     chips = detector.extract_image_chips(img, points, 144, 0.37)
+#     for i, chip in enumerate(chips):
+#         cv2.imshow('chip_'+str(i), chip)
+#         cv2.imwrite('chip_'+str(i)+'.png', chip)
 
-    for p in points:
-        for i in range(5):
-            cv2.circle(draw, (p[i], p[i + 5]), 1, (0, 0, 255), 2)
+#     draw = img.copy()
+#     for b in total_boxes:
+#         cv2.rectangle(draw, (int(b[0]), int(b[1])), (int(b[2]), int(b[3])), (255, 255, 255))
 
-    cv2.imshow("detection result", draw)
-    cv2.waitKey(0)
+#     for p in points:
+#         for i in range(5):
+#             cv2.circle(draw, (p[i], p[i + 5]), 1, (0, 0, 255), 2)
+
+#     cv2.imshow("detection result", draw)
+#     cv2.waitKey(0)
 
 # --------------
 # test on camera
 # --------------
-'''
+
 camera = cv2.VideoCapture(0)
 while True:
     grab, frame = camera.read()
-    img = cv2.resize(frame, (320,180))
+    # img = cv2.resize(frame, (320,180))
+    img = cv2.resize(frame, (640,360))
+    # img = frame
 
     t1 = time.time()
     results = detector.detect_face(img)
@@ -63,4 +65,4 @@ while True:
             cv2.circle(draw, (p[i], p[i + 5]), 1, (255, 0, 0), 2)
     cv2.imshow("detection result", draw)
     cv2.waitKey(30)
-'''
+
